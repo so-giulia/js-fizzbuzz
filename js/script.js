929b1b2ffa8f72a894e20449fab641e256dd14e2
GiuliaSoccolini <giuliasoccolini@yahoo.it>
2021-06-03 15:34:52 +0200

    add: FizzBuzz (3)

--- js/script.js
+++ js/script.js
@@ -3,10 +3,11 @@
 // Scrivi un programma che stampi i num. da 1 a 100
 // Per i multipli di 3 deve stampare anche "Fizz" di fianco al numero
 // Per i multipli di 5 deve stampare anche "Buzz" di fianco al numero
+// Per i multipli di 3 e di 5 stampare anche "FizzBuzz" di fianco al numero
 
 for (var i = 1; i < 101; i++){ 	
 
-    if(i % 15 == 0){
+    if(i % 3 == 0 && i % 5 == 0){
         console.log((i) + " " + "FizzBuzz"); 
     }else if(i % 3 == 0){
         console.log((i) + " " + "Fizz"); 
@@ -15,5 +16,6 @@ for (var i = 1; i < 101; i++){
     }else{
         console.log(i); 
     } 
+
 }
 
