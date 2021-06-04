aa6b0e74c8653d4eaf96d1e0c2ad05da2c499a37
GiuliaSoccolini <giuliasoccolini@yahoo.it>
2021-06-04 10:06:03 +0200

    add: correzione in classe

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@
 // Per i multipli di 5 deve stampare anche "Buzz" di fianco al numero
 // Per i multipli di 3 e di 5 stampare anche "FizzBuzz" di fianco al numero
 
-for (var i = 1; i < 101; i++){ 	
+//Mio vecchio
+
+/* for (var i = 1; i < 101; i++){ 	
 
     if(i % 3 == 0 && i % 5 == 0){
         console.log((i) + " " + "FizzBuzz"); 
@@ -17,5 +19,24 @@ for (var i = 1; i < 101; i++){
         console.log(i); 
     } 
 
-}
+} */
 
+
+// Nuovo in classe corretto
+
+for (var i = 1; i <= 100; i++){ 	
+
+    document.getElementById("output").innerHTML += i + " ";
+
+    if(i % 3 == 0){
+        document.getElementById("output").innerHTML += "Fizz";
+    }
+    if(i % 5 == 0){
+        document.getElementById("output").innerHTML += "Buzz";
+    }
+    if(i % 6 == 0){
+        document.getElementById("output").innerHTML += "Boolean";
+    }
+
+    document.getElementById("output").innerHTML += "<br>";
+}
