966dbecdfbe7aba4523964033e8659ec984859b6
“Giulia <soccolinigiulia@gmail.com>
2021-10-10 15:40:52 +0200

    cooler fizzbuzz

--- js/script.js
+++ js/script.js
@@ -4,39 +4,7 @@
 // Per i multipli di 3 deve stampare anche "Fizz" di fianco al numero
 // Per i multipli di 5 deve stampare anche "Buzz" di fianco al numero
 // Per i multipli di 3 e di 5 stampare anche "FizzBuzz" di fianco al numero
+// Per i multipli di 6 stampare anche "Boolean" di fianco al numero
 
-//Mio vecchio
-
-/* for (var i = 1; i < 101; i++){ 	
-
-    if(i % 3 == 0 && i % 5 == 0){
-        console.log((i) + " " + "FizzBuzz"); 
-    }else if(i % 3 == 0){
-        console.log((i) + " " + "Fizz"); 
-    }else if (i % 5 == 0){
-        console.log((i) + " " + "Buzz"); 
-    }else{
-        console.log(i); 
-    } 
-
-} */
-
-
-// Nuovo in classe corretto
-
-for (var i = 1; i <= 100; i++){ 	
-
-    document.getElementById("output").innerHTML += i + " ";
-
-    if(i % 3 == 0){
-        document.getElementById("output").innerHTML += "Fizz";
-    }
-    if(i % 5 == 0){
-        document.getElementById("output").innerHTML += "Buzz";
-    }
-    if(i % 6 == 0){
-        document.getElementById("output").innerHTML += "Boolean";
-    }
-
-    document.getElementById("output").innerHTML += "<br>";
-}
+var output = document.getElementById("output");
+for(i=1; i<= 100; i++)output.innerHTML+=i+' '+(i%3?'':' Fizz ')+(i%5?'':' Buzz ')+(i%6?'':' Boolean ');
